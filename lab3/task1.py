f934fda383afeba2b78000910e69726004d59f43
Danil-Paramonov <14prostodanya88@gmail.com>
2024-11-06 22:53:40 +0300

    lab3
    Здраствуйте! У меня возникла проблема:
    При проверке ответа на 2-ое задание выдаёт ошибку, при этом логически ответ соотвествует ТЗ, выведен список общих участников в алфавитном порядке

---
+++ lab3/task1.py
@@ -0,0 +1,16 @@
+def find_index(bag, product):
+    a = 0
+    if product in bag:
+        a = bag.index(product)
+    else:
+        a = None
+    return a
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
